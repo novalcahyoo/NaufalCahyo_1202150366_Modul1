471c77d1f473d7a29e2ee549c344f8535373ccd1
novalcahyoo <36079118+novalcahyoo@users.noreply.github.com>
2018-02-04 21:15:37 +0700

    Update MainActivity.java

--- app/src/main/java/com/naufalcahyo/modul1/naufalcahyo_1202150366_modul1/MainActivity.java
+++ app/src/main/java/com/naufalcahyo/modul1/naufalcahyo_1202150366_modul1/MainActivity.java
@@ -3,80 +3,32 @@ package com.naufalcahyo.modul1.naufalcahyo_1202150366_modul1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
-    private EditText menuu,jumlahh;
-    private TextView tempatt;
-    private int total,bayar;
-    private int duit = 65500;
-    private int hrgApnormal = 30000;
-    private int hrgEatbus = 50000;
-
+    private TextView restaurant, menu, jumlah, bayar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
 
-        menuu = (EditText) findViewById(R.id.menu);
-        tempatt = (TextView) findViewById(R.id.restaurant);
-        jumlahh = (EditText) findViewById(R.id.jumlah);
-
-    }
-
-    public void visitapnormal(View view) {
-
-        String menu = menuu.getText().toString();
-        String jumlah = jumlahh.getText().toString();
-
-        total = Integer.parseInt(jumlah.trim());
-        bayar = total * hrgApnormal;
-
-        Intent intent = new Intent(this,SecondActivity.class);
-
-        intent.putExtra("Menu", menu);
-        intent.putExtra("Jumlah",jumlah);
-        intent.putExtra("Total",String.valueOf(bayar));
-
-        startActivity(intent);
-
-        if (bayar < duit){
-            Toast.makeText(this,"Disini aja makan malamnya",Toast.LENGTH_LONG).show();
-        }else{
-            Toast.makeText(this,"Jangan disini makan malamnya, berat," +
-                    " uang kamu tidak cukup",Toast.LENGTH_LONG).show();
-        }
-    }
+        restaurant = (TextView) findViewById(R.id.restaurant);
+        menu = (TextView) findViewById(R.id.menu);
+        jumlah = (TextView) findViewById(R.id.jumlah);
+        bayar = (TextView) findViewById(R.id.bayar);
 
 
-    public void visiteatbus(View view) {
+        if (getIntent().getExtras() != null) {
 
+            Bundle bundle = getIntent().getExtras();
 
-        String menu = menuu.getText().toString();
-        String jumlah = jumlahh.getText().toString();
-
-        total = Integer.parseInt(jumlah.trim());
-        bayar = total * hrgEatbus;
-
-        Intent intent = new Intent(this,SecondActivity.class);
-
-        intent.putExtra("Menu", menu);
-        intent.putExtra("Jumlah",jumlah);
-        intent.putExtra("Total",String.valueOf(bayar));
-
-        startActivity(intent);
-
-        if (bayar > duit){
-            Toast.makeText(this,"Jangan disini makan malamnya, berat," +
-                    " uang kamu tidak cukup",Toast.LENGTH_LONG).show();
-        }else{
-            Toast.makeText(this,"Disini aja makan malamnya",Toast.LENGTH_LONG).show();
+            restaurant.setText(bundle.getString("Restaurant"));
+            menu.setText(bundle.getString("Menu"));
+            jumlah.setText(bundle.getString("Jumlah"));
+            bayar.setText(bundle.getString("Total"));
         }
     }
 }
